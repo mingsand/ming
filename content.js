77b54912c90ab181c68800b2d9eb612d53bf24d9
ming_sand <yumysandwich077@gmail.com>
2024-07-25 16:25:26 +0900

    Add files via upload

---
+++ content.js
@@ -0,0 +1,5 @@
+     if (window.location.href.startsWith("https://playentry.org/community/entrystory")) {
+      console.log("Target URL matched");
+      alert("밍.. 나가세요...");
+      window.history.back();
+     }
